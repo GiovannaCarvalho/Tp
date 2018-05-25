a75391af43910b74c622a3eb1b7902cdc1d65ee8
GiovannaCarvalho <39504976+GiovannaCarvalho@users.noreply.github.com>
2018-05-25 13:25:44 -0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -43,93 +43,116 @@ float frac(char numero[],int basen,int tam) {
 	return soma;
 }*/
 
-void binto(char *numero, int base) {
+void binto(char *numero_somente, int base2,char *numero) {
 	int a;
-	if (base == 8) {
+	long int reminder,octal,j;
+	float soma=0;
+	int *aux;
+	printf("entrei\n");
+	if (base2 == 8) {
 		int cont = 0;
 		int i;
-		for (i = 0; numero[i] != ' '; i++) {
+		printf("entrei uhu\n");
+		for (int i = 0; numero[i] != ' '; i++) {
+		    printf("%d",i);
 			cont++;
+			printf("%d",cont);
 		}
-		int *aux = (int*)malloc(sizeof(int) * cont); //mesma coisa que "int aux[cont];"
-		for (i = 0; i > cont; i++) {
-			aux[i] = *numero[i];
+		aux/*[cont];*/ = (int*)malloc(sizeof(int) * cont); //mesma coisa que "int aux[cont];"*/
+		for (i = 0; i < cont; i++) {
+			aux[i] = numero_somente[i];
 		}
 		for (i = 0; i < cont; i++) {
-			
+		    printf("entrei uhuuu");
+		    while(numero_somente[i]!=0){
+			    reminder=numero_somente[i]%10;
+			    printf("%d:",reminder);
+			    octal=octal+reminder*j;
+			    printf("%d:",octal);
+			    j=j*2;
+			    printf("%d:",j);
+			    numero_somente[i]=numero_somente[i]/10;
+			    printf("%s:",numero_somente[i]);
+		    }
 		}
 	}
-	if (base == 10) {
+	if (base2 == 10) {
+		a = 0;		
+	}
+	if (base2 == 16) {
 		a = 0;
 	}
-	if (base == 16) {
-		a = 0;
-	}
+	free (aux);
 }
 
 void decide(char *numero) {
+
+	char numero_somente[50];
 	int i;
 	int base, base2;//base  a converter;
+
 	for (i = 0; numero[i] != ' '; i++) {
+		numero_somente[i] = numero[i];
 	}
 	i++;
-	if (numero[i] == 2) {
-		for (; numero[i] != ' '; i++) {
+	if (numero[i] == '2') {
+		for (int cont=i++; numero[cont] != ' '; cont++) {
 		}
 		i++;
-		if (numero[i] == 8) {
+		if (numero[i] == '8') {
 			base2 = 8;
-			//converteb(numero, 8);
+			printf("a\n");
+			binto(numero_somente, base2,numero);
 		}
-		if (numero[i + 1] == 0) {
+		if (numero[i + 1] == '0') {
 			base2 = 10;
 		}
-		if (numero[i + 1] == 6) {
+		if (numero[i + 1] == '6') {
 			base2 = 16;
 		}
 	}
-	if (numero[i] == 8) {
+	if (numero[i] == '8') {
 		for (; numero[i] != ' '; i++) {
 		}
 		i++;
-		if (numero[i] == 2) {
+		if (numero[i] == '2') {
 			base2 = 2;
 			//converteb(numero, 8);
 		}
-		if (numero[i + 1] == 0) {
+		if (numero[i + 1] == '0') {
 			base2 = 10;
 		}
-		if (numero[i + 1] == 6) {
+		if (numero[i + 1] == '6') {
 			base2 = 16;
 		}
 	}
-	if (numero[i + 1] == 0 && numero[i + 2] != ' ') {
+	if (numero[i + 1] == '0' && numero[i + 2] != ' ') {
 		for (; numero[i] != ' '; i++) {
 		}
 		i++;
-		if (numero[i] == 8) {
+		if (numero[i] == '8') {
 			base2 = 8;
 			//converteb(numero, 8);
 		}
-		if (numero[i] == 2) {
+		if (numero[i] == '2') {
 			base2 = 2;
 		}
-		if (numero[i + 1] == 6) {
+		if (numero[i + 1] == '6') {
 			base2 = 16;
 		}
 	}
-	if (numero[i + 1] == 6 && numero[i + 2] != ' ') {
+	if (numero[i + 1] == '6' && numero[i + 2] != ' ') {
 		for (; numero[i] != ' '; i++) {
 		}
 		i++;
-		if (numero[i] == 8) {
+		if (numero[i] == '8') {
 			base2 = 8;
 			//converteb(numero, 8);
 		}
-		if (numero[i + 1] == 0) {
+		if (numero[i + 1] == '0') {
 			base2 = 10;
 		}
-		if (numero[i] == 2) {
+		if (numero[i] == '2') {
 			base2 = 2;
 		}
 	}
@@ -139,19 +162,19 @@ int main() {
 	char numero[50];
 	int basen, baset, tam;
 	float c, f;
-	printf("Programa de coversão de Bases\n");
+	printf("Programa de coversï¿½o de Bases\n");
 	printf("Funcionamento: \n");
-	printf("(sinal)(numero)\"ESPAÇO\"(base do numero)\"ESPAÇO\"(base a converter): \n");
-	//Corrigir a leitura 
+	printf("(sinal)(numero)\"ESPAï¿½O\"(base do numero)\"ESPAï¿½O\"(base a converter): \n");
 	fgets(numero, 50, stdin);
 	decide(numero);
+	//printf("%s\n",numero );
 	/*tam = strlen(numero);
 	c=convertdec(numero,tam,basen);
 	f=frac(numero,tam,basen);
 	printf("%i.%f", c, f);
-	//fputs(numero, stdout);
+	fputs(numero, stdout);
 	//printf(" base: %i base t:%i\n",basen,baset);
 	*/
-	system("pause");
+	//system("pause");
 	return 0;
 }
